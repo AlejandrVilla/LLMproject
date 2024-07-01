56b1b1af7bbbe8a0454209c9f4fcba69cfc0dc04
AlejandrVilla <avillah@unsa.edu.pe>
2024-07-01 13:21:33 -0500

    refactor: Change to generate multiple plan for the user to choose

--- get_recomendation/app.py
+++ get_recomendation/app.py
@@ -3,64 +3,20 @@ load_dotenv('./env.txt')
 from flask import Flask, request, json, jsonify
 from flask_cors import CORS, cross_origin
 import requests
-from langchain.prompts import (
-    PromptTemplate,
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    AIMessagePromptTemplate,
-)
-from langchain_core.messages import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
-from langchain_openai import ChatOpenAI
 from gmaps_utils import (
     get_places, 
     get_geocode, 
     get_coord, 
-    get_place_info
+    get_place_summary
 )
-from langchain.chains import LLMChain, SimpleSequentialChain
 import os
 import json
-
+import random
 from pprint import pprint
 
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-# recomendation templates
-recomendation_system_message = "You are a helpful assistant. You will receive some places with extra information: distances and times to get, ratings and comments."
-recomendation_system_template = "You will give greater importance to the places by {order_by}"
-recomendation_human_template_1 = "Prepare a plan to do an activity using one place for each type of activitie in the same order from the following list {places_info}, if there are no such place, print: \"no place availables for the activity\""
-recomendation_human_template_2 = "Add the following summary of the reviews to your answer: {reviews_summary}"
-
-recomendation_chat_prompt = ChatPromptTemplate.from_messages(
-    [
-        SystemMessage(content=recomendation_system_message),
-        SystemMessagePromptTemplate.from_template(recomendation_system_template),
-        HumanMessagePromptTemplate.from_template(recomendation_human_template_1),
-        HumanMessagePromptTemplate.from_template(recomendation_human_template_2)
-    ]
-)
-
-# get from summary reviews microservice
-def get_summary_reviews(all_places_reviews):
-    summary_reviews_url = "http://summary-reviews:5003/summary-reviews"
-    # summary_reviews_url = "http://127.0.0.1:5003/summary_reviews"
-    all_places_reviews = {"all_places_reviews": all_places_reviews}
-    headers = {"Content-type": "application/json"}
-    response = requests.post(summary_reviews_url, headers=headers, data=json.dumps(all_places_reviews))
-    if response.status_code != 200:
-        res = "Error trying to connect to summary reviews microservice"
-        return 0, res
-    res = response.json()
-    reviews_summary = res["reviews_summary"]
-
-    return 1, reviews_summary
 
 def get_filter_prediction(activities):
     filter_url = "http://filter:5004/filter"
@@ -82,7 +38,7 @@ def get_filter_prediction(activities):
 
 def get_extract_places(activities, language):
     places_url = "http://extract-places:5002/extract-places"
-    # places_url = "http://127.0.0.1:5002/extract_places"
+    # places_url = "http://127.0.0.1:5002/extract-places"
     activities_dict = {
         "activities": activities,
         "language": language
@@ -97,18 +53,22 @@ def get_extract_places(activities, language):
     places_type = res["places_type"]
     return 1, places_type
 
-# get response
-def get_response(order_by, places_info, reviews_summary, temperature: float = 0.7):
-    chat_openai = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature = temperature)
-
-    recomendations_messages = recomendation_chat_prompt.format(
-        order_by=order_by,
-        places_info=places_info,
-        reviews_summary=reviews_summary
-    )
-
-    response = chat_openai.invoke(recomendations_messages)
-    return response
+def post_group_places(group_places, places_type):
+    places_url = "http://get-plan:5005/post-plan"
+    # places_url = "http://127.0.0.1:5005/post-plan"
+    group_places_dict = {
+        "group_places": group_places,
+        "places_type": places_type
+    }
+    headers = {"Content-type": "application/json"}
+    response = requests.post(places_url, headers=headers, data=json.dumps(group_places_dict))
+    if response.status_code != 200:
+        res = "Error trying to connect post-plan method in get-plan microservice"
+        return 0, res
+    
+    res = response.json()
+    message = res["message"]
+    return 1, message
 
 # Microservice route
 @app.route('/get-recomendation', methods=['POST'])
@@ -143,13 +103,17 @@ def get_recomendation():
     #     response.status_code = 200
     #     return response
 
-    reference_place = data.get('reference_place')
-    order_by = data.get('order_by')
     origin = data.get('origin')
     radius = data.get('radius')
-    mode = data.get('mode')
     language = data.get('language')
     temperature = data.get('temperature')
+
+    print("user data:")
+    print(f"activities: {activities}")
+    print(f"origin: {origin}")
+    print(f"radius: {radius}")
+    print(f"language: {language}")
+    print(f"temperature: {temperature}")
 
     # Return activities from extract_places microservice
     error, places_type = get_extract_places(activities=activities, language=language)
@@ -162,65 +126,81 @@ def get_recomendation():
         return response
 
     # Using Google maps
-    reference_place_geocode = get_geocode(reference_place)
-    reference_place_coord = get_coord(reference_place_geocode)
     origin_geocode = get_geocode(origin)
     origin_coord = get_coord(origin_geocode)
     all_places_info = {}
-    all_places_reviews = {}
 
+    max_places = 0
     for query_place in places_type:
         nearby_places, place_names, place_ids = get_places(
             query_place=query_place,
-            coord=reference_place_coord,
+            coord=origin_coord,
             radius=radius,
             language=language
         )
-
-        places_info, places_reviews = get_place_info(
-            origin=origin_coord,
-            place_names=place_names,
-            place_ids=place_ids,
-            mode=mode
-        )
+        places_info = []
+        # max 4 elements
+        for i in range(min(len(place_names), 3)):
+            place_info = get_place_summary(
+                place_name=place_names[i],
+                place_id=place_ids[i],
+            )
+            places_info.append(place_info)
         all_places_info[query_place] = places_info
-        all_places_reviews[query_place] = places_reviews
+        if(len(places_info) > max_places):
+            max_places = len(places_info)
 
     # Save data
-    # json_object2 = json.dumps(all_places_info, indent=4)
-    # with open("all-places-50-m.json", "w") as outfile:
-    #     outfile.write(json_object2)
-    # json_object2 = json.dumps(all_places_reviews, indent=4)
-    # with open("all-reviews-50-m.json", "w") as outfile:
-    #     outfile.write(json_object2)
-        
-    # Get summary reviews from microservice
-    # all_places_reviews = ""
-    # all_places_info = ""
-    error, reviews_summary = get_summary_reviews(
-        all_places_reviews = all_places_reviews
+    json_object = json.dumps(all_places_info, indent=4)
+    with open("places_info.json", "w") as outfile:
+        outfile.write(json_object)
+
+    group_places = []
+    # max 4 groups recomendations
+    for i in range(min(3, max_places)):
+        places = []
+        # for each place extract one place randomly
+        for query_place in places_type:
+            place = {}
+            if(len(all_places_info[query_place]) == 0):
+                place = {"type": query_place}
+            else:
+                ind = random.randint(0, len(all_places_info[query_place])-1)
+                place = all_places_info[query_place][ind]
+                place["type"] = query_place
+            places.append(place)
+        group_places.append(places)
+
+    # with open('group_places.json', 'r') as file:
+    #     group_places = json.load(file)
+    # with open('places_type.json', 'r') as file:
+    #     places_type = json.load(file)
+
+    # Return activities from extract_places microservice
+    error, message = post_group_places(
+        group_places=group_places, 
+        places_type=places_type
     )
     if error == 0:
         # Format data
-        res_dic = {"content": reviews_summary}
-        # print(res_dic)
-        response = jsonify(res_dic)
+        res_dict = {"content": message}
+        # print(res_dict)
+        response = jsonify(res_dict)
         response.status_code = 200
         return response
-
-    # Get LLM response
-    # reviews_summary = ""
-    res = get_response(
-        order_by=order_by,
-        places_info=all_places_info,
-        reviews_summary=reviews_summary,
-        temperature=temperature
-    )
+    print(message)
 
     # Format data
-    res_dict = {"content": res.content}
+    res_dict = {"content": group_places}
     
-    print(res_dict)
+    json_object = json.dumps(group_places, indent=4)
+    with open("group_places.json", "w") as outfile:
+        outfile.write(json_object)
+    json_object = json.dumps(places_type, indent=4)
+    with open("places_type.json", "w") as outfile:
+        outfile.write(json_object)
+    
+    # print(res_dict)
     response = jsonify(res_dict)
     response.status_code = 200
 
--- get_recomendation/gmaps_utils.py
+++ get_recomendation/gmaps_utils.py
@@ -15,7 +15,7 @@ def get_geocode(reference_place):
 def get_coord(geocode):
     return geocode[0]['geometry']['location']
 
-########### extract places
+########### extract places for get-recomendation
 def get_places(query_place, coord, radius: int = 25, language: str="spanish"):
     # with places
     # places = gmaps.places(query=query_place, location=coord, radius=radius, open_now=False, language=language)
@@ -26,10 +26,11 @@ def get_places(query_place, coord, radius: int = 25, language: str="spanish"):
     nearby_places = []
     place_names = []
     place_ids = []
-    for place in places['results']:
-        nearby_places.append({place['place_id']: place['name']})
-        place_names.append(place['name'])
-        place_ids.append(place['place_id'])
+    if places['status'] != "ZERO_RESULTS":
+        for place in places['results']:
+            nearby_places.append({place['place_id']: place['name']})
+            place_names.append(place['name'])
+            place_ids.append(place['place_id'])
 
     return nearby_places, place_names, place_ids
 
@@ -42,31 +43,71 @@ def get_directions(origin, place_id, mode: str = "walking"):
         
     return distance, duration
 
-########### extract place info
-def get_place_info(origin, place_names, place_ids, mode: str = "walking"):
-    places_info = []
-    places_reviews = []
+########### extract place info for get-recomendation
+def get_place_summary(place_name, place_id):
+    place_info = {}
+    place = gmaps.place(place_id=place_id)
+    try:
+        rating = place['result']['rating']
+    except KeyError:
+        rating = None
+    try:
+        maps_url = place['result']['url']
+    except KeyError:
+        maps_url = None
+    try:
+        phone_number = place['result']['international_phone_number']
+    except KeyError:
+        phone_number = None
+    try:
+        opened = place['result']['current_opening_hours']['open_now']
+    except KeyError:
+        opened = None
+    place_info = {
+        "name": place_name,
+        "place_id": place_id,
+        "rating": rating,
+        "maps_url": maps_url,
+        "phone_number": phone_number,
+        "opened": opened
+    }
 
-    for i in range(min(len(place_names),1)):
-        place_info = {}
-        distance, duration = get_directions(origin, place_ids[i], mode=mode)
-        place = gmaps.place(place_id=place_ids[i])
-        try:
-            rating = place['result']['rating']
-        except KeyError:
-            rating = None
-        try:
-            reviews = place['result']['reviews']
-            text_reviews = []
-            for review in reviews:
-                text_reviews.append(review["text"])
-        except KeyError:
-            text_reviews = None
-        place_info["distance"] = distance
-        place_info["duration"] = duration
-        place_info["rating"] = rating
-        places_info.append({place_names[i]: place_info})
-        places_reviews.append({place_names[i]: text_reviews})
+    # json_object = json.dumps(place_info, indent=4)
+    # with open("places_info.json", "w") as outfile:
+    #     outfile.write(json_object)
+
+    return place_info
+
+########### extract place info for get-plan
+def get_place_info(origin, place_name, place_id, mode: str = "walking"):
+    place_reviews = []
+    place_info = {}
+
+    distance, duration = get_directions(origin, place_id, mode=mode)
+    place = gmaps.place(place_id=place_id)
+    try:
+        rating = place['result']['rating']
+    except KeyError:
+        rating = None
+    try:
+        location = place['result']['geometry']['location']
+    except KeyError:
+        location = None
+    try:
+        reviews = place['result']['reviews']
+        text_reviews = []
+        for review in reviews:
+            text_reviews.append(review["text"])
+    except KeyError:
+        text_reviews = None
+    
+    place_info["name"] = place_name
+    place_info["distance"] = distance
+    place_info["duration"] = duration
+    place_info["rating"] = rating
+    place_info["location"] = location
+    place_info["mode"] = mode
+    place_reviews = text_reviews
 
     # json_object = json.dumps(places_info, indent=4)
     # with open("places-50-m.json", "w") as outfile:
@@ -75,4 +116,4 @@ def get_place_info(origin, place_names, place_ids, mode: str = "walking"):
     # with open("reviews-50-m.json", "w") as outfile:
     #     outfile.write(json_object2)
 
-    return places_info, places_reviews
+    return place_info, place_reviews
